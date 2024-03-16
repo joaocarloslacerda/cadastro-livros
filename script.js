905427a4a448a7a588844ca18f086d9f13c3a0f8
joao <juniorlacerdasilva@gmail.com>
2024-03-16 17:43:37 -0300

    iniciada function que fará a request de post na API livros

---
+++ script.js
@@ -0,0 +1,9 @@
+function cadastrarLivro(){
+    
+    const tituloLivro = document.getElementById("titutloLivro")
+    const descricaoLivro = document.getElementById("descricaoLivro")
+
+    const url = "https://api-aula.up.railway.app/livros"
+
+    
+}
