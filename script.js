ad44df970211d1324cf3b39b103668e241b17367
joao <juniorlacerdasilva@gmail.com>
2024-03-17 00:49:03 -0300

    criadas funções e adicionada request POST para cadastrar livros

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-function cadastrarLivro(){
+async function cadastrarLivro(){
     
-    const tituloLivro = document.getElementById("titutloLivro")
-    const descricaoLivro = document.getElementById("descricaoLivro")
+    const tituloLivro = document.getElementById("tituloLivro").value
+    const descricaoLivro = document.getElementById("descricaoLivro").value
+
+    montaJson(tituloLivro, descricaoLivro)
+
+    console.log(tituloLivro, descricaoLivro)
+    
+}
+async function montaJson(tituloLivro, descricaoLivro){
 
     const url = "https://api-aula.up.railway.app/livros"
 
-    
+    const body = {
+         title: tituloLivro,
+         description: descricaoLivro
+    }
+
+    await requestPost(url, body)
+
+    console.log(url, body)
+
+}
+async function requestPost(url, body){
+
+    const retornoFetch = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {"Content-type": "application/json"}
+    })
+
+    const retornoJson = await retornoFetch.json()
+    console.log(retornoJson)
 }
